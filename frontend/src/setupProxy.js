20311ba9f32369eb8d5d96670df4d664d61008f9
katzurk <kanickakt@gmail.com>
2025-06-25 16:40:32 +0200

    minor css adjustments, setupProxy fix

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: process.env.REACT_APP_BACKEND_URL || "http://backend:4000",
+      target: process.env.REACT_APP_BACKEND_URL,
       changeOrigin: true,
     })
   );
